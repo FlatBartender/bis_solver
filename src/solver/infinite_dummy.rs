28eccee2aaf1b051a416d415c585fb933b5d3723
Flat Bartender <flat.bartender@gmail.com>
2022-09-19 10:49:23 +0200

    Fix infinite dummy evaluator with newer timeline improvements

--- src/solver/infinite_dummy.rs
+++ src/solver/infinite_dummy.rs
@@ -4,35 +4,58 @@ use crate::data::Gearset;
 
 pub trait InfiniteDummyStat: crate::data::StatRepo {
     fn cycle_length(&self) -> f64 {
-        self.cycle_normal_gcd() as f64 * self.adjusted_gcd() + 2.5
+        self.casts_per_cycle() as f64 * self.adjusted_gcd() + 2.5
     }
 
-    fn cycle_normal_gcd(&self) -> f64 {
-        ((30.0 - 2.5) / self.adjusted_gcd()).round()
+    fn casts_per_cycle(&self) -> f64 {
+        let gcd = self.adjusted_gcd();
+        let casts_per_cycle = (30.0 - 2.5) / gcd;
+        let early_refresh_casts = casts_per_cycle.floor();
+        let late_refresh_casts = casts_per_cycle.ceil();
+
+        let early_refresh_pps = {
+            let cycle = gcd * early_refresh_casts + 2.5;
+            let dosis_per_second = early_refresh_casts / cycle;
+            let ticks_per_second = cycle.min(30.0) / cycle / 3.0;
+            dosis_per_second * 330.0 + ticks_per_second * 70.0
+        };
+        let late_refresh_pps = {
+            let cycle = gcd * late_refresh_casts + 2.5;
+            let dosis_per_second = late_refresh_casts / cycle;
+            let ticks_per_second = cycle.min(30.0) / cycle / 3.0;
+            dosis_per_second * 330.0 + ticks_per_second * 70.0
+        };
+        if early_refresh_pps >= late_refresh_pps {
+            // We're on an early refresh cycle
+            early_refresh_casts
+        } else {
+            // We're on a late refresh cycle
+            late_refresh_casts
+        }
     }
 
-    fn cycle_phlegma(&self) -> f64 {
-        self.cycle_length() * 2.0 / 3.0 / 30.0
+    fn phlegma_per_cycle(&self) -> f64 {
+        self.cycle_length() / 45.0
     }
 
-    fn cycle_dosis(&self) -> f64 {
-        self.cycle_normal_gcd() - self.cycle_phlegma()
+    fn dosis_per_cycle(&self) -> f64 {
+        self.casts_per_cycle() - self.phlegma_per_cycle()
     }
 
-    fn cycle_eukr_dosis(&self) -> f64 {
-        1.0
+    fn eukr_dosis_ticks_per_cycle(&self) -> f64 {
+        self.cycle_length().min(30.0) / 3.0
     }
 
-    fn dosis_aps(&self) -> f64 {
-        self.cycle_dosis() / self.cycle_length()
+    fn dosis_per_second(&self) -> f64 {
+        self.dosis_per_cycle() / self.cycle_length()
     }
 
-    fn phlegma_aps(&self) -> f64 {
-        self.cycle_phlegma() / self.cycle_length()
+    fn phlegma_per_second(&self) -> f64 {
+        self.phlegma_per_cycle() / self.cycle_length()
     }
 
-    fn eukr_dosis_aps(&self) -> f64 {
-        self.cycle_eukr_dosis() / self.cycle_length()
+    fn eukr_dosis_ticks_per_second(&self) -> f64 {
+        self.eukr_dosis_ticks_per_cycle() / self.cycle_length()
     }
 
     fn dosis_score(&self) -> f64 {
@@ -56,22 +79,20 @@ pub trait InfiniteDummyStat: crate::data::StatRepo {
         let map = self.magic_attack_power();
         let det = self.det_multiplier();
         let sps = self.sps_multiplier();
-        let ticks_lost_per_cast = (30.0 - self.cycle_length()).abs() / 3.0;
-        let expected_tick_number = 10.0 * (1.0 - ticks_lost_per_cast) + 9.0 * ticks_lost_per_cast;
         let damage = 70.scale(adj_wd).scale(map).scale(det).scale(sps) * 130 / 100 + 1;
-        damage as f64 * self.crit_factor() * self.dh_factor() * expected_tick_number
+        damage as f64 * self.crit_factor() * self.dh_factor()
     }
 
     fn pps(&self) -> f64 {
-        self.dosis_aps() * 330.0
-            + self.phlegma_aps() * 510.0
-            + self.eukr_dosis_aps() * 700.0
+        self.dosis_per_second() * 330.0
+            + self.phlegma_per_second() * 510.0
+            + self.eukr_dosis_ticks_per_second() * 700.0
     }
 
     fn dps(&self) -> f64 {
-        self.dosis_aps() * self.dosis_score()
-        + self.phlegma_aps() * self.phlegma_score()
-        + self.eukr_dosis_aps() * self.eukr_dosis_score()
+        self.dosis_per_second() * self.dosis_score()
+        + self.phlegma_per_second() * self.phlegma_score()
+        + self.eukr_dosis_ticks_per_second() * self.eukr_dosis_score()
     }
 }
 
